a0b424f371152747a2cca8de2e42322d83008539
Ahmed Osama Mohamed <42383306+ahmedosamataha@users.noreply.github.com>
2021-07-15 18:01:04 +0200

    pushing show error and correct error
    adding show error and correct error functions and buttons

--- widget.cpp
+++ widget.cpp
@@ -60,4 +60,150 @@ void Widget::on_saveButton_clicked()
 
 }
 
+//adding get error in tts position by commenting that with recommendation to add the error
+void Widget::showErrors(){
+    QString B;
+    QStack<QString> tagStack;
+    QString tag;
+    B = ui->textEdit->toPlainText();
+    QString C;
+    int x = B.length();
+    for(int i = 0; i < x; i++){
+        if(B[i] != '<'){
+            C.append(B[i]);
+        }
+        if(B[i] == '/' && B[i+1] == '>'){
+            qDebug()<< "No Error " <<tagStack.pop();
+            continue;
+        }
+        if(B[i] == '<'){
+            if(B[i+1] == '!' || B[i+1] == '?'){
+                C.append("<");
+                continue;
+            }
+            else if(B[i+1] == '/'){
+                QString subStr2 = B.mid(i+2, -1);
+                if(!tagStack.isEmpty() && subStr2.mid(0, subStr2.indexOf(">")) != tagStack.top()){
+                    C.append("<!--*******Error: Close Tag: ");
+                    C.append(tagStack.top() + "******************-->");
+                    C.append("\n");
+                    //C.append("<");
+                    qDebug() << "Error: Close Tag: " <<tagStack.pop();
+                }
+                if(subStr2.mid(0, subStr2.indexOf(">")) == tagStack.top()){
+                    C.append("<");
+                    qDebug() << "NO ERROR: " <<tagStack.pop();
+                    continue;
+                }
+            }
+            else{
+                C.append("<");
+                QString subStr = B.mid(i+1, -1);
+                if(subStr.indexOf(" ") < subStr.indexOf(">") && subStr.indexOf(" ") < subStr.indexOf("/")){
+                    tag = subStr.mid(0,subStr.indexOf(" "));
+                    tagStack.push(tag);
+                }
+                else if (subStr.indexOf(" ") > subStr.indexOf(">")){
+                    tag = subStr.mid(0,subStr.indexOf(">"));
+                    tagStack.push(tag);
+                }
+            }
+        }
+    }
+    while(!tagStack.isEmpty()){
+        C.append("<!--*******Error: Close Tag: ");
+        C.append(tagStack.top() + "****************-->");
+        C.append("\n");
+        //C.append("<");
+
+        qDebug() << "Error: Close Tag" <<tagStack.pop();
+    }
+    ui->textEdit_2->clear();
+    ui->textEdit_2->setPlainText(C);
+
+}
+
+
+//fixing the error 
+void Widget::fixErrors(){
+    QString B;
+    QStack<QString> tagStack;
+    QString tag;
+    B = ui->textEdit->toPlainText();
+    QString C;
+    int x = B.length();
+    for(int i = 0; i < x; i++){
+        if(B[i] != '<'){
+            C.append(B[i]);
+        }
+        if(B[i] == '/' && B[i+1] == '>'){
+            qDebug()<< "No Error " <<tagStack.pop();
+            continue;
+        }
+        if(B[i] == '<'){
+            if(B[i+1] == '!' || B[i+1] == '?'){
+                C.append("<");
+                continue;
+            }
+            else if(B[i+1] == '/'){
+                QString subStr2 = B.mid(i+2, -1);
+                //qDebug() << "CHECK THIS "<<subStr2.mid(0, subStr2.indexOf(">"));
+                if(!tagStack.isEmpty() && subStr2.mid(0, subStr2.indexOf(">")) != tagStack.top()){
+                    //error
+                    C.append("<!--******* ADDING Close Tag: ");
+                    C.append(tagStack.top() + "******************-->");
+                    C.append("\n");
+                    C.append("</" + tagStack.top() + ">");
+                    C.append("\n");
+                    //C.append("<");
+                    qDebug() << "Error: Close Tag: " <<tagStack.pop();
+                }
+                if(subStr2.mid(0, subStr2.indexOf(">")) == tagStack.top()){
+                    C.append("<");
+                    qDebug() << "NO ERROR: " <<tagStack.pop();
+                    continue;
+
+                }
+            }
+            else{
+                C.append("<");
+                QString subStr = B.mid(i+1, -1);
+                if(subStr.indexOf(" ") < subStr.indexOf(">") && subStr.indexOf(" ") < subStr.indexOf("/")){
+                    tag = subStr.mid(0,subStr.indexOf(" "));
+                    tagStack.push(tag);
+                }
+                else if (subStr.indexOf(" ") > subStr.indexOf(">")){
+                    tag = subStr.mid(0,subStr.indexOf(">"));
+                    tagStack.push(tag);
+                }
+            }
+        }
+    }
+    while(!tagStack.isEmpty()){
+        C.append("<!--*******ADDING Tag: ");
+        C.append(tagStack.top() + "****************-->");
+        C.append("\n");
+        C.append("</" + tagStack.top() + ">");
+        C.append("\n");
+        //C.append("<");
+
+        qDebug() << "Error: Close Tag" <<tagStack.pop();
+    }
+    ui->textEdit_2->clear();
+    ui->textEdit_2->setPlainText(C);
+}
+
+//adding the buttons to call the function
+void Widget::on_gotoAnother_clicked()
+{
+    showErrors();
+    qDebug()<< "***********************************";
+}
+
+void Widget::on_Fix_button_clicked()
+{
+    fixErrors();
+    qDebug()<< "***********************************";
+}
+
 
